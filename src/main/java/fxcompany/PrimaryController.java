e7ec263521ea79c760ca80dfde8c929afd0d0fc8
Кривотулов Алексей Владимирович <AKrivotulov@tsc.ts>
2020-02-27 15:28:37 +0300

    Initial commit

---
+++ src/main/java/fxcompany/PrimaryController.java
@@ -0,0 +1,12 @@
+package fxcompany;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
