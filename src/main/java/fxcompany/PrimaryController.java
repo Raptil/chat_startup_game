5bdd527ef412f9b9710821966f61d003920b1d4e
Raptil <Hurricane095@gmail.com>
2020-02-28 17:13:21 +0300

    add send on client

--- src/main/java/fxcompany/PrimaryController.java
+++ src/main/java/fxcompany/PrimaryController.java
@@ -2,11 +2,28 @@ package fxcompany;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
+    private TextArea chatArea;
+
+    @FXML
+    private TextField chatField;
+
+    @FXML
+    private Button send;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void sendMessage() throws IOException{
+        chatArea.appendText(chatField.getText());
     }
 }
