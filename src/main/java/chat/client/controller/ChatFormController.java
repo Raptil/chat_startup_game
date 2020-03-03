9960f7ba39a1fa459f0db0b6deb0979a8ec292b9
Pushkin Sergey <pushkinsergey.voronezh@gmail.com>
2020-03-03 17:37:39 +0300

    Clean up chat form controller

--- src/main/java/chat/client/controller/ChatFormController.java
+++ src/main/java/chat/client/controller/ChatFormController.java
@@ -1,14 +1,5 @@
 package chat.client.controller;
 
-import chat.client.App;
-import javafx.fxml.FXML;
-
-import java.io.IOException;
-
 public class ChatFormController {
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
 }
